ce36ccc604ca6809b87a1ba8b65e8ff11e90865b
sayed mudassir <sayedmuddassir68@gmail.com>
2024-09-20 20:40:01 +0000

    h3

--- src/components/App.js
+++ src/components/App.js
@@ -57,7 +57,7 @@ export const App = ()=>{
                 data-testid='clear'
                 onClick={clear}
                 >Clear</button>
-            {<h3>{relation}</h3>}
+            {relation && <h3>{relation}</h3>}
         </div>
     )
 };
